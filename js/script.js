b5fc53e3a1a150277f4f0b644a4b97904fd9be90
unknown <rafayuno@gmail.com>
2023-03-07 15:17:30 -0300

    Página home finalizada

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 const body = document.querySelector('body');
+const portuguese = document.querySelector('.pt-br');
+const english = document.querySelector('.en-us');
+const text = document.querySelector('.text');
+
+english.addEventListener('click', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    text.textContent = 'A JOURNEY TROUGH THE LIFE OF A WEB DEVELOPER'
+})
+
+portuguese.addEventListener('click', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    text.textContent = 'A JORNADA PELA VIDA DE UM PROGRAMADOR WEB';
+})
